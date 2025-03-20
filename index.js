7e295e4d24523fd492523fcfbef2bab8202df1ca
samuel8772 <kamauskk005@gmail.com>
2025-03-20 11:47:07 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // 31 * 2 = 62
+
+const random = Math.floor(Math.random() * 100) + 1; // Ensures a random integer > 0
+
+const num3 = 28;
+const num4 = 6;
+const mod = num3 % num4; // 28 % 6 = 4
+
+const max = Math.max(5, 10, 15, 20); // Ensures 20 is the highest number
